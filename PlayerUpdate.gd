ec58cd477fa7586d95d5ae212b554dc62d4202a5
AqtoPy <rekikreki66@gmail.com>
2025-06-16 18:32:40 +0300

    Update PlayerUpdate.gd

--- PlayerUpdate.gd
+++ PlayerUpdate.gd
@@ -9,3 +9,17 @@ func _on_summit_area_entered(area):
 func _on_summit_area_exited(area):
     if area.is_in_group("summit"):
         is_on_summit = false
+
+
+
+# В скрипте игрока
+var flag_scene = preload("res://Flag.tscn")
+
+func place_flag(mountain_name):
+    var new_flag = flag_scene.instantiate()
+    new_flag.set_name(mountain_name)
+    get_tree().current_scene.add_child(new_flag)
+    new_flag.global_transform.origin = global_transform.origin
+    
+    # Сохраняем в базу данных (простой вариант)
+    save_mountain_data(mountain_name, global_transform.origin)
